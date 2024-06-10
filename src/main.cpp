c1323d55b8da242c66c25842a9da949d453b173d
rm -rf /* <maziknewacc@gmail.com>
2024-06-10 13:52:04 +0300

    asio add

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,32 @@
-//TODO: everything xD
+#include "asio/error_code.hpp"
+#include "asio/io_context.hpp"
+#include "asio/ip/address.hpp"
+#include "asio/ip/tcp.hpp"
+#include <iostream>
+
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+int main()
+{
+	asio::error_code ec;
+	asio::io_context context;
+
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+	asio::ip::tcp::socket socket(context);
+
+	socket.connect(endpoint, ec);
+	
+	if (!ec)
+	{
+		std::cout << "Connected" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to connect" << ec.message() << std::endl;
+	}
+	//TODO: everything xD
+    return 0;
+}
